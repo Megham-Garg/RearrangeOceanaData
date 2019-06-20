dc787c6bcc59e688ecd0bd117f5d80522912cbf9
Megham-Garg <gargmegham3@gmail.com>
2019-06-20 14:02:05 +0530

    20.06.2019

--- MongoDB_Data_Entry.py
+++ MongoDB_Data_Entry.py
@@ -3,47 +3,44 @@ import pandas as pd
 import math
 import threading
 
-client= pm.MongoClient('localhost',27017)	#create client to connect to the server
-db= client.new_database				#create new database
-usersM= db.usersM				#create collection for Mumbai30-50.xlsx
-usersTR= db.usersTR				#create collection for Tonk-Rajasthan.xlsx
-usersSM= db.usersSM				#create collection for Sawai-Madhopur.xlsx
-usersSMR= db.usersSMR				#create collection for Sawai-Madhopur-Rajasthan.xlsx
+client= pm.MongoClient('localhost',27017) #make a client to connect to mongodb default port
+db= client.new_database1    #create a database
+users= db.users             #create a collection
 
-SM_xl= pd.ExcelFile('Sawai-Madhopur.xlsx')
-TR_xl= pd.ExcelFile('Tonk-Rajasthan.xlsx')
-df1=pd.read_excel('Sawai-Madhopur-Rajasthan.xlsx')
-list_of_fields=list(pd.read_excel('Sawai-Madhopur-Rajasthan.xlsx').columns)
+SM_xl= pd.ExcelFile('Sawai-Madhopur.xlsx')  #create a dataframe in pandas
+TR_xl= pd.ExcelFile('Tonk-Rajasthan.xlsx')  #create a dataframe in pandas
+df1=pd.read_excel('Sawai-Madhopur-Rajasthan.xlsx')  #create a dataframe in pandas
+list1=df1.to_dict(orient='records') #create a list of dictionaries in python
+list_of_fields=list(pd.read_excel('Sawai-Madhopur-Rajasthan.xlsx').columns) #create a list of fields in Sawai-Madhopur-Rajasthan.xlsx
+
+#remove these fields from the list
 list_of_fields.remove('Name of the Subscriber')
 list_of_fields.remove('Telephone Number')
 list_of_fields.remove('ID')
 
-#create of list of dictionaries
-list1=df1.to_dict(orient='records')	
-list2=pd.read_excel('Mumbai30-50.xlsx').to_dict(orient='records')
-list3=pd.read_excel(TR_xl,'Sheet1').to_dict(orient='records')
-list4=pd.read_excel(TR_xl,'Sheet2').to_dict(orient='records')
+list2=pd.read_excel('Mumbai30-50.xlsx').to_dict(orient='records')   #create a list of dictionaries in python
+list3=pd.read_excel(TR_xl,'Sheet1').to_dict(orient='records')   #create a list of dictionaries in python
+list4=pd.read_excel(TR_xl,'Sheet2').to_dict(orient='records')   #create a list of dictionaries in python
 list5=list()
 for i in  range(10):
-    list5+=pd.read_excel(SM_xl,'Sheet{}'.format(i+1)).to_dict(orient='records') #create of list of dictionaries
+    list5+=pd.read_excel(SM_xl,'Sheet{}'.format(i+1)).to_dict(orient='records')     #create a list of dictionaries in python
 
 '''
-to order the data of list2
+order data of each dictionary passed for Mumbai30-50.xlsx
 '''
 def ordered_dictM(d):
     d['Age']= d.pop('Age')
     d['Phone Number']= d.pop('Mobile No.')
-    if(type(d['Phone Number'])==int):
+    if(type(d['Phone Number'])==int):           #check validity of input
         if(d['Phone Number'] > 9999999999999):
             d['Phone Number']='Invalid'
     if(type(d['Phone Number'])==str):
-        if(len(d['Phone Number']) > 13):
+        if(len(d['Phone Number']) > 13):        #check validity of input
             d['Phone Number']='Invalid'
     return d
 
-
 '''
-to order the data of list3
+order data of each dictionary passed for Tonk-Rajasthan.xlsx sheet1
 '''
 def ordered_dictTR1(d):
     d['Name']= d.pop('Customer Name')
@@ -52,9 +49,8 @@ def ordered_dictTR1(d):
     d['City']= d.pop('City')
     return d
 
-
 '''
-to order the data of list4
+order data of each dictionary passed for Tonk-Rajasthan.xlsx sheet2
 '''
 def ordered_dictTR2(d):
     d['First Name']= d.pop('Name')
@@ -70,7 +66,7 @@ def ordered_dictTR2(d):
 
 
 '''
-to order the data of list1
+order data of each dictionary passed for Sawai-Madhopur-Rajasthan.xlsx
 '''
 def ordered_dictSMR(d):
     d['Name']= d.pop('Name of the Subscriber')
@@ -80,16 +76,18 @@ def ordered_dictSMR(d):
         d[item]=d.pop(item)
     return d
 
+'''
+check if any field of a dictionary is empty
+'''
 def checkEmpty(d):
     for key in d:
-        if(type(d[key])==int):
+        if(type(d[key])==int):      #to avoid TypeError
             if(math.isnan(d[key])):
                 d[item]=''
     return d
 
-
 '''
-to order the data of list5
+order data of each dictionary passed for Sawai-Madhopur.xlsx
 '''
 def ordered_dictSM(d):
     for ok in ['Name','Gender','mobile','address']:
@@ -98,42 +96,47 @@ def ordered_dictSM(d):
                 d[ok]= d.pop(ok)
     return d
 
-#insert data into corresponding collections
+'''
+to insert data in the collection users in new_database1 in MongoDB
+'''
 def insert5(l):
     for item in l:
-        db.usersSM.insert_one(ordered_dictSM(item))
+        db.users.insert_one(ordered_dictSM(item))
 
 def insert2(l):
     for item in l:
-        db.usersM.insert_one(ordered_dictM(item))
+        db.users.insert_one(ordered_dictM(checkEmpty(item)))
 
 def insert3(l):
     for item in l:
-        db.usersTR.insert_one(ordered_dictTR1(item))
+        db.users.insert_one(ordered_dictTR1(checkEmpty(item)))
 
 def insert4(l):
     for item in l:
-        db.usersTR.insert_one(ordered_dictTR2(item))
+        db.users.insert_one(ordered_dictTR2(checkEmpty(item)))
 
 def insert1(l):
     for item in l:
-        db.usersSMR.insert_one(ordered_dictSMR(checkEmpty(item)))
+        db.users.insert_one(ordered_dictSMR(checkEmpty(item)))
 
+#will run if not imported
 if __name__ == "__main__":
+
+    #define threads where target is function and args is arguements to be passed
     t1=threading.Thread(target=insert1,args=(list1,))
     t2=threading.Thread(target=insert2,args=(list2,))
     t3=threading.Thread(target=insert3,args=(list3,))
     t4=threading.Thread(target=insert4,args=(list4,))
     t5=threading.Thread(target=insert5,args=(list5,))
 
-    #start the thread
+    #start threads
     t1.start()
     t2.start()
     t3.start()
     t4.start()
     t5.start()
 
-    #end the thread
+    #join threads when done
     t1.join()
     t2.join()
     t3.join()
